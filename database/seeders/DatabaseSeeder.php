3ddefd33e9fbf8c0163c498cd65571517dcaf624
Muhammad Babun Waseptian <waseptian26@gmail.com>
2026-01-22 03:53:17 +0700

    Update Seeder: Tambah user admin default

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash; // WAJIB: Import ini untuk enkripsi password
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Membuat User Admin Manual
+        User::create([
+            'name'     => 'Administrator',      // Nama lengkap (bebas)
+            'username' => 'admin',              // Username Login
+            'password' => Hash::make('admin123'), // Password (Wajib di-Hash)
         ]);
     }
 }
